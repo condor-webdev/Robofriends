14dc63ddd8b7768411463389dc984d16b0e4cbf4
Stéphane Metz <condor.webdev@gmail.com>
2019-11-21 00:54:47 +0100

    Updates

---
+++ precache-manifest.4fac7c042341342ca01d5f70cf88b6ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef263f0926820e28891ada093e0cc549",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3731f37d432a50d35095",
+    "url": "/Robofriends/static/css/main.5e90ca73.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/Robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "3731f37d432a50d35095",
+    "url": "/Robofriends/static/js/main.b9a7b743.chunk.js"
+  },
+  {
+    "revision": "a3eff8e1727534c00f37",
+    "url": "/Robofriends/static/js/runtime-main.11a3c7d6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
